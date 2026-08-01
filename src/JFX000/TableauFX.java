f5b6a936a3be3f8328b888087287091d40e88d1a
jsa1512 <jsa1512@gmail.com>
2026-08-01 20:54:02 +0200

    ajout boutons 7,8,9

---
+++ src/JFX000/TableauFX.java
@@ -0,0 +1,45 @@
+package JFX000;
+
+
+
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+
+public class TableauFX extends Pane{
+	
+	private int largeur, hauteur;
+	private Pane pane;
+	
+	public TableauFX(int largeur, int hauteur) {
+		super();
+		this.largeur = largeur;
+		this.hauteur = hauteur;
+		GridPane grid = new GridPane();
+		
+		Button button1 = new Button("Button 1");
+        Button button2 = new Button("Button 2");
+        Button button3 = new Button("Button 3");
+        Button button4 = new Button("Button 4");
+        Button button5 = new Button("Button 5");
+        Button button6 = new Button("Button 6");
+        Button button7 = new Button("Button 7");
+        Button button8 = new Button("Button 8");
+        Button button9 = new Button("Button 9");
+
+
+        grid.add(button1, 0, 0, 1, 1);
+        grid.add(button2, 1, 0, 1, 1);
+        grid.add(button3, 2, 0, 1, 1);
+        grid.add(button4, 0, 1, 1, 1);
+        grid.add(button5, 1, 1, 1, 1);
+        grid.add(button6, 2, 1, 1, 1);
+        grid.add(button7, 0, 2, 1, 1);
+        grid.add(button8, 1, 2, 1, 1);
+        grid.add(button9, 2, 2, 1, 1);
+        
+        this.getChildren().add(grid);
+        
+	}
+
+}
